7169484a7702efbc24fd63d3c4521c0a0ec09837
tauhiddewan <dewan.tauhid.rahman@gmail.com>
2023-04-16 12:00:35 +0600

    updated scripts

--- train.py
+++ train.py
@@ -1,12 +1,14 @@
 import pickle 
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
-from utils import features_path, models_path
+from sklearn.ensemble import RandomForestRegressor
+from utils import features_path, models_path, cfg
 
 X_train = pd.read_csv(str(features_path / "train_features.csv"))
 y_train = pd.read_csv(str(features_path / "train_labels.csv"))
 
-model = LinearRegression()
+# model = LinearRegression()
+model = RandomForestRegressor(n_estimators=150, max_depth=6, random_state=cfg["RANDOM_SEED"])
 model = model.fit(X=X_train, y=y_train)
 
 pickle.dump(model, open(str(models_path / "model.pickle"), "wb"))
