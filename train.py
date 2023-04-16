d24979e2728f32c8d0b7df50e442e20ca0923ecd
tauhiddewan <dewan.tauhid.rahman@gmail.com>
2023-04-16 12:31:23 +0600

    added parameter import from yaml

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import pickle 
+import yaml
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
@@ -7,8 +8,11 @@ from utils import features_path, models_path, cfg
 X_train = pd.read_csv(str(features_path / "train_features.csv"))
 y_train = pd.read_csv(str(features_path / "train_labels.csv"))
 
-# model = LinearRegression()
-model = RandomForestRegressor(n_estimators=150, max_depth=6, random_state=cfg["RANDOM_SEED"])
+params = yaml.safe_load(open("params.yaml"))["train"]
+n_estimators = params["n_estimators"]
+max_depth = params["max_depth"]
+
+model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=cfg["RANDOM_SEED"])
 model = model.fit(X=X_train, y=y_train)
 
 pickle.dump(model, open(str(models_path / "model.pickle"), "wb"))
